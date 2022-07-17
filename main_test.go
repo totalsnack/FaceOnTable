b2ebbb1b3aa54f8205297c79b4ea1e1785faa56f
Eugene <yevhen.bilyk@dev>
2022-07-17 18:03:40 +0300

    bugs fixed

--- main_test.go
+++ main_test.go
@@ -20,17 +20,16 @@ func TestDo(t *testing.T) {
 				res string
 				err error
 			}{
-				"testing a, 1, lower":  {args: args{"a", 1, false}, res: "[a1]", err: nil},
-				"testing b, 2, upper":  {args: args{"b", 2, true}, res: "[B2]", err: nil},
-				"testing d, 21, upper": {args: args{"d", 21, true}, res: "D", err: nil},
-				"testing d, 13, lower": {args: args{"d", 13, false}, res: "[d2]", err: nil},
-				"testing z, 1, lower":  {args: args{"z", 1, false}, res: "", err: fmt.Errorf("invalid s")},
-				"testing f, 0, lower":  {args: args{"f", 0, false}, res: "", err: fmt.Errorf("invalid i")},
+				"testing a, 1, lower":      {args: args{"a", 1, false}, res: "[1a]", err: nil},
+				"testing b, 2, upper":      {args: args{"b", 2, true}, res: "[2B]", err: nil},
+				"testing d, 21, upper":     {args: args{"d", 21, true}, res: "D", err: nil},
+				"testing d, 13, lower":     {args: args{"d", 13, false}, res: "d", err: nil},
+				"testing z, 1, lower":      {args: args{"z", 1, false}, res: "1", err: fmt.Errorf("invalid s")},
+				"testing d, 100500, lower": {args: args{"d", 100500, false}, res: "", err: fmt.Errorf("invalid i")},
 			}
 			for testName, test := range testCases {
 				t.Run(testName, func(t *testing.T) {
-					if got, err := doer.Do(test.s, test.i, test.b); got != test.res && err != test.err {
-						fmt.Println("!!!!!!!!!!!!!!!", got, err)
+					if got, err := doer.Do(test.s, test.i, test.b); got != test.res || err != test.err {
 						t.Fail()
 					}
 				})
